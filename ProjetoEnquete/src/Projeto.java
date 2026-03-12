166f1dc88832aaca4145a94cb92afb0a4e7b5836
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-12 11:59:03 -0300

    primeira versao do prejeto enquete

---
+++ ProjetoEnquete/src/Projeto.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Projeto{
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+
+        System.out.println("1 qual é o seu nome?");
+        String nome = tec.nextLine();
+
+
+        System.out.println("2 defina classe e objeto");
+        String classeobjeto = tec.nextLine();
+
+        System.out.println("3 qual é a função do construtor?");
+        String construtor = tec.nextLine();
+
+        System.out.println("4 qual é a sua maior dificudade ate o momento");
+        String dificuldades = tec.nextLine();
+        
+
+
+        System.out.println("nome");
+
+
+    }
+
+}
+
---
+++ teste.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+   private string nome;
+   private int idade;
+
+   public string getNome() {
+       return nome;
+
+
+   }
+}
